c1174928d72b91d60a20e82a8abe471b7051b57b
hasnainmawia <hasnainmawia.w@gmail.com>
2025-04-29 20:28:59 +0500

    OOPS- Practice

--- index.php
+++ index.php
@@ -31,11 +31,41 @@ $conn = db_connect();
             $email = $_SESSION['email'];
             $query = mysqli_query($conn, "SELECT users . * FROM `users` WHERE users.email = '$email'");   
              while($data = mysqli_fetch_array($query)){
-                echo $data['first_name'] . '' . $data['last_name'];
+                // echo $data['first_name'] . ' ' . $data['last_name'];
              }   
                }?>
         </p>
         <a title="Logout" href="logout.php" class="btn">Logout</a>
     </div>
+
+    <?php
+               
+    class player{
+        public $name;
+        public $email;
+
+        // Method
+       function set_name($name){
+        $this->name = $name; 
+       } 
+       function get_name(){
+        return $this->name; 
+       } 
+    }           
+    echo "Lets Work on OOPs <br>";
+    $player1 = new player();
+    $player1->set_name('Hasnain Mawia <br>');
+    echo $player1->get_name();
+    $player1 = new player();
+    $player1->set_name('Osama Sheikh <br>');
+    echo $player1->get_name();
+    $player1 = new player();
+    $player1->set_name('Anas Khan <br>');
+    echo $player1->get_name();
+
+
+?>
+
+
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-require_once './includes/config.php';
+require_once './includes/config.php';-
 session_destroy();
 header('location:'.SITEURL.'login.php');
 
